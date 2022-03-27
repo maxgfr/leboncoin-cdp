cf4f289feb386beedcdcfae65652121c5e95988b
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-03-27 13:12:16 +0200

    fix: dev

--- src/headless.ts
+++ src/headless.ts
@@ -1,10 +1,13 @@
 import puppeteer from 'puppeteer';
 
-export async function getScreenshot(url: string): Promise<Buffer | string> {
+export async function takeScreenshot(
+  url: string,
+  filename: string,
+): Promise<Buffer | string> {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
-  const buffer = await page.screenshot();
+  const buffer = await page.screenshot({ path: `./assets/${filename}.png` });
   await browser.close();
   return buffer;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import 'dotenv/config'; // To use our .env
-import { getScreenshot } from './headless';
+import { takeScreenshot } from './headless';
 
 async function main() {
-  const res = await getScreenshot('https://www.leboncoin.fr');
-  console.log(res);
+  await takeScreenshot('https://www.leboncoin.fr', 'homepage');
 }
 
 main();
