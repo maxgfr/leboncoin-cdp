37ca83bfc9f53e7cab925de3cfdc13cef9634777
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-03-27 22:43:52 +0200

    fix: screenshot

--- src/headless.ts
+++ src/headless.ts
@@ -1,13 +1,20 @@
-import puppeteer from 'puppeteer';
+import Apify from 'apify';
 
 export async function takeScreenshot(
   url: string,
   filename: string,
-): Promise<Buffer | string> {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(url);
-  const buffer = await page.screenshot({ path: `./assets/${filename}.png` });
-  await browser.close();
-  return buffer;
+  chromePath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+) {
+  Apify.main(async () => {
+    const browser = await Apify.launchPuppeteer({
+      stealth: true,
+      launchOptions: {
+        executablePath: chromePath,
+      },
+    });
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.screenshot({ path: `./assets/${filename}.png` });
+    await browser.close();
+  });
 }
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,10 @@ import 'dotenv/config'; // To use our .env
 import { takeScreenshot } from './headless';
 
 async function main() {
-  await takeScreenshot('https://www.leboncoin.fr', 'homepage');
+  await takeScreenshot(
+    'https://www.leboncoin.fr/recherche?category=9&locations=Clermont-Ferrand__45.78574122226367_3.0939572793408208_9154&owner_type=private&real_estate_type=1%2C2',
+    'homepage',
+  );
 }
 
 main();
