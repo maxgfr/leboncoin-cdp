f652a47cd440db1e4ee60729bee0b6e759c3bbfa
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-03-28 22:01:37 +0200

    fix: exploit

--- src/headless.ts
+++ src/headless.ts
@@ -1,22 +1,35 @@
-import Apify from 'apify';
-import { SearchResult } from './types';
+import fs from 'fs';
+import puppeteer from 'puppeteer-extra';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+import AdblockerPlugin from 'puppeteer-extra-plugin-adblocker';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-expect-error
+import AnonymUa from 'puppeteer-extra-plugin-anonymize-ua';
 
-export async function getAllSearchResult(
+puppeteer.use(AdblockerPlugin());
+puppeteer.use(StealthPlugin());
+puppeteer.use(AnonymUa());
+
+export async function saveAllSearchResult(
   query: string,
+  saveContent = {
+    fileName: 'scrap',
+  },
   chromePath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-): Promise<SearchResult[]> {
-  return new Promise((resolve) => {
-    Apify.main(async () => {
-      const browser = await Apify.launchPuppeteer({
-        stealth: true,
-        launchOptions: {
-          executablePath: chromePath,
-        },
-      });
-      const page = await browser.newPage();
-      await page.goto('https://www.leboncoin.fr/recherche?' + query);
-      await browser.close();
-      resolve([{ title: 'test' }]);
-    });
+): Promise<void> {
+  const browser = await puppeteer.launch({
+    executablePath: chromePath,
+    headless: false,
   });
+
+  const page = await browser.newPage();
+  await page.goto('https://www.leboncoin.fr/recherche?' + query);
+
+  const pageContent = await page.content();
+
+  if (saveContent) {
+    fs.writeFileSync(`./assets/${saveContent.fileName}.html`, pageContent);
+  }
+
+  await browser.close();
 }
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import 'dotenv/config'; // To use our .env
-import { getAllSearchResult } from './headless';
+import { exploitSearchContent } from './exploit';
+import fs from 'fs';
 
 async function main() {
-  await getAllSearchResult(
-    'category=9&locations=Clermont-Ferrand__45.78574122226367_3.0939572793408208_9154_5000',
-  );
+  // await saveAllSearchResult(
+  //   'category=9&locations=Clermont-Ferrand__45.78574122226367_3.0939572793408208_9154_5000',
+  // );
+  exploitSearchContent(fs.readFileSync('assets/scrap.html', 'utf8'));
 }
 
 main();
