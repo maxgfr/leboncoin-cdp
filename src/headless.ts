71b0145278e34fa1cab74e7093f5950fc4d69fcb
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-03-31 19:29:00 +0200

    fix: scrapping

--- src/headless.ts
+++ src/headless.ts
@@ -5,6 +5,7 @@ import AdblockerPlugin from 'puppeteer-extra-plugin-adblocker';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 //@ts-expect-error
 import AnonymUa from 'puppeteer-extra-plugin-anonymize-ua';
+import { exploitSearchContent } from './exploit';
 
 puppeteer.use(AdblockerPlugin());
 puppeteer.use(StealthPlugin());
@@ -16,6 +17,7 @@ export async function saveAllSearchResult(
     fileName: 'scrap',
   },
   chromePath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+  resultPerPage = 35,
 ): Promise<void> {
   const browser = await puppeteer.launch({
     executablePath: chromePath,
@@ -32,6 +34,24 @@ export async function saveAllSearchResult(
       `./assets/${saveExternalContent.fileName}.html`,
       pageContent,
     );
+  }
+
+  const result = exploitSearchContent(pageContent, {
+    fileName: 'res1',
+  });
+
+  const nbPages = Math.ceil(result.total / resultPerPage);
+
+  if (nbPages > 1) {
+    for (let i = 2; i <= nbPages; i++) {
+      await page.goto(
+        'https://www.leboncoin.fr/recherche?' + query + `&page=${i}`,
+      );
+
+      exploitSearchContent(await page.content(), {
+        fileName: 'res' + i,
+      });
+    }
   }
 
   await browser.close();
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import 'dotenv/config'; // To use our .env
 import { exploitSearchContent } from './exploit';
 import fs from 'fs';
+import { saveAllSearchResult } from './headless';
 
 async function main() {
-  // await saveAllSearchResult(
-  //   'category=9&locations=Clermont-Ferrand__45.78574122226367_3.0939572793408208_9154_5000',
-  // );
-  exploitSearchContent(fs.readFileSync('assets/scrap.html', 'utf8'));
+  await saveAllSearchResult(
+    'category=9&locations=Clermont-Ferrand__45.78574122226367_3.0939572793408208_9154_5000',
+  );
+  // exploitSearchContent(fs.readFileSync('assets/scrap.html', 'utf8'));
 }
 
 main();
